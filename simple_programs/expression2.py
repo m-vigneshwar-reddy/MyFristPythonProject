6430db424fa4b3fd83a8e6e81a0655740f150c94
m-vigneshwar-reddy <m.vigneshwer7780@gmail.com>
2024-08-13 10:10:35 +0530

    maked some changes to the expression2.py program

--- simple_programs/expression2.py
+++ simple_programs/expression2.py
@@ -13,21 +13,21 @@ import re
 check = False
 while check != True :
     name = input("Enter your name(without any special characters) : ")
-    if re.search(r'[A-Z||a-z||\s||\_]*', name) != None:
+    if re.search('[\w\s]*', name) != None:
         check = True
     else:
         print("the name should not have any special charters (-,:;'\"<>?/+= ) ")
 check = False
 while check == False :
     number = input("Enter your Number(please add pin code at start) : ")
-    if re.search('[\+91][0-9]{10}',number) != None:
+    if re.search('[\+91][\d]{10}',number) != None:
         check = True
     else:
         print("This Number is not from India!!! please Enter an Indian Number ")
 check = False
 while check == False :
     E_mail = input("Enter the E_mail id :")
-    if re.search('[a-zA-Z0-9\_]*[@][G||g][mail.com]',E_mail) != None:
+    if re.search('[\w]*[@][Gg][mail.com]',E_mail) != None:
         check = True
     else:
         print("these E_mail id is worng ")
@@ -35,7 +35,7 @@ check = False
 while check == False :
     password = input("Enter the password :")
     c_password = input ("confrom password(re-enter the password) : ")
-    if re.search('[a-zA-z0-9[^a-zA-z0-9]]+{6,}[\s]*',password) != None and password ==c_password :
+    if re.search('\w\W\s]+{6,}',password) != None and password ==c_password :
         check = True
     elif(password !=c_password) :
         print("please make shoull that you Enter same password")
